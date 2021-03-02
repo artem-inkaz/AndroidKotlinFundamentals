445ef1eabad356eb06bdae68f4a338eadb4f6883
artemternovenko <artem.inkaz@gmail.com>
2021-03-02 22:40:51 +0600

    Initial commit

---
+++ app/src/main/java/com/example/androidkotlinfundamentals/TitleFragment.kt
@@ -0,0 +1,28 @@
+package com.example.androidkotlinfundamentals
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.androidkotlinfundamentals.databinding.FragmentTitleBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class TitleFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
+            R.layout.fragment_title,container,false)
+        return binding.root
+    }
+}
