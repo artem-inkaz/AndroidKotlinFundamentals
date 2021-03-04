4ef19eee46668ca777a64345411c3a4dc7b0e3e1
artemternovenko <artem.inkaz@gmail.com>
2021-03-04 10:29:22 +0600

    [FIXED] Manifest

--- app/src/main/java/com/example/androidkotlinfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/androidkotlinfundamentals/MainActivity.kt
@@ -3,13 +3,6 @@ package com.example.androidkotlinfundamentals
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.MenuItem
-import androidx.databinding.DataBindingUtil
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
-import com.example.androidkotlinfundamentals.R
-import com.example.androidkotlinfundamentals.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,5 +15,4 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         Log.i("MainActivity","onStart Called")
     }
-
 }
