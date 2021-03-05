a1985a4e6e789b15dc3e2ec24106aec30dfef8d1
artemternovenko <artem.inkaz@gmail.com>
2021-03-05 18:32:45 +0600

    [ADDED] Build Project

--- app/src/main/java/com/example/androidkotlinfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/androidkotlinfundamentals/MainActivity.kt
@@ -6,38 +6,12 @@ import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
-    // Contains all the views
-//   private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-//     val binding = DataBindingUtil.setContentView(this, R.layout.main_activity)
+        setContentView(R.layout.activity_main)
+
     }
 
-    override fun onStart() {
-        super.onStart()
- //       Log.i("MainActivity","onStart Called")
-        Timber.i("onCreate called")
-    }
-    override fun onResume() {
-        super.onResume()
-        Timber.i("onResume Called")
-    }
-    override fun onPause() {
-        super.onPause()
-        Timber.i("onPause Called")
-    }
-    override fun onStop() {
-        super.onStop()
-        Timber.i("onStop Called")
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        Timber.i("onDestroy Called")
-    }
-    override fun onRestart() {
-        super.onRestart()
-        Timber.i("onRestart Called")
-    }
+
 }
