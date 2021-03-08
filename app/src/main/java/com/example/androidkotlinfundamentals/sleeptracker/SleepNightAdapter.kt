f158811147dd15f791202bf1775e67fbea1246b6
artemternovenko <artem.inkaz@gmail.com>
2021-03-08 17:10:45 +0600

    [ADDED]   Inflate the item layout using data binding Replace findViewById() Create binding adapters (SleepNightAdapater.kt) Update SleepNightAdapter Add bindings to XML layout

--- app/src/main/java/com/example/androidkotlinfundamentals/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/androidkotlinfundamentals/sleeptracker/SleepNightAdapter.kt
@@ -1,21 +1,18 @@
 package com.example.androidkotlinfundamentals.sleeptracker
 
-import android.graphics.Color
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.ListAdapter
-import android.widget.TextView
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidkotlinfundamentals.R
-import com.example.androidkotlinfundamentals.TextItemViewHolder
 import com.example.androidkotlinfundamentals.convertDurationToFormatted
 import com.example.androidkotlinfundamentals.convertNumericQualityToString
 import com.example.androidkotlinfundamentals.database.SleepNight
+import com.example.androidkotlinfundamentals.databinding.ListItemSleepNightBinding
+import com.example.androidkotlinfundamentals.sleeptracker.SleepNightAdapter.ViewHolder.Companion.from
 
-class SleepNightAdapter: ListAdapter<SleepNight,SleepNightAdapter.ViewHolder>(SleepNightDiffCallBack()){
+class SleepNightAdapter: ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(SleepNightDiffCallBack()){
 //        var data = listOf<SleepNight>()
    // To tell the RecyclerView when the data that it's displaying has changed,
    // add a custom setter to the data variable that's at the top of the SleepNightAdapter class.
@@ -34,8 +31,8 @@ class SleepNightAdapter: ListAdapter<SleepNight,SleepNightAdapter.ViewHolder>(Sl
    // in the same RecyclerView. For example, if you put a list of text views, an image,
    // and a video all in the same RecyclerView, the onCreateViewHolder() function would need
    // to know what type of view to use.
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-       return RecyclerView.ViewHolder.from(parent)
+   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+       return ViewHolder.from(parent)
    }
 
     // function is called by RecyclerView to display the data for one list item
@@ -53,62 +50,46 @@ class SleepNightAdapter: ListAdapter<SleepNight,SleepNightAdapter.ViewHolder>(Sl
     // Change the signature of the ViewHolder class so that the constructor is private.
     // Because from() is now a method that returns a new ViewHolder instance, there's no reason for
     // anyone to call the constructor of ViewHolder anymore.
-    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView){
-        // get references to the views. You need a reference to the views that this ViewHolder will update.
-        // Every time you bind this ViewHolder, you need to access the image and both text views.
-// (You convert this code to use data binding later.)
-        val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
-        val quality: TextView = itemView.findViewById(R.id.quality_string)
-        val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
+    class ViewHolder private constructor(val binding: ListItemSleepNightBinding) : RecyclerView.ViewHolder(binding.root){
 
         fun bind(item: SleepNight) {
-            val res = itemView.context.resources
-            sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-            quality.text = convertNumericQualityToString(item.sleepQuality, res)
-            qualityImage.setImageResource(when (item.sleepQuality) {
-                0 -> R.drawable.ic_sleep_0
-                1 -> R.drawable.ic_sleep_1
-                2 -> R.drawable.ic_sleep_2
-                3 -> R.drawable.ic_sleep_3
-                4 -> R.drawable.ic_sleep_4
-                5 -> R.drawable.ic_sleep_5
-                else -> R.drawable.ic_sleep_active
-            })
+            binding.sleep = item
+            binding.executePendingBindings()
     }
 
     companion object {
         fun from(parent: ViewGroup): ViewHolder {
             // create an instance of LayoutInflater.
             val layoutInflater = LayoutInflater.from(parent.context)
+            // Added binding
             // create the view by asking the layoutinflater to inflate it
-            val view = layoutInflater
-                .inflate(R.layout.list_item_sleep_night, parent, false)
+//            val view = layoutInflater
+//                .inflate(R.layout.list_item_sleep_night, parent, false)
+            val binding = ListItemSleepNightBinding.inflate(layoutInflater,parent,false)
             // return a TextItemViewHolder made with view
-            return ViewHolder(view)
+            return ViewHolder(binding)
         }
       }
     }
-
-    class SleepNightDiffCallBack : DiffUtil.ItemCallback<SleepNight>(){
-        override fun areItemsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
-            // To change body of created functions use File | Settings | File Templates.
-            // If the items have the same nightId, they are the same item, so return true. Otherwise,
-            // return false. DiffUtil uses this test to help discover if an item was added, removed, or moved.
-            return  oldItem.nightId == newItem.nightId
-        }
-
-        override fun areContentsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
-            // To change body of created functions use File | Settings | File Templates.
-            // Inside areContentsTheSame(), check whether oldItem and newItem contain the same data;
-            // that is, whether they are equal. This equality check will check all the fields,
-            // because SleepNight is a data class. Data classes automatically define equals and
-            // a few other methods for you. If there are differences between oldItem and newItem,
-            // this code tells DiffUtil that the item has been updated.
-            return oldItem == newItem
-        }
-
-    }
 }
 
+class SleepNightDiffCallBack : DiffUtil.ItemCallback<SleepNight>(){
+    override fun areItemsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+        // To change body of created functions use File | Settings | File Templates.
+        // If the items have the same nightId, they are the same item, so return true. Otherwise,
+        // return false. DiffUtil uses this test to help discover if an item was added, removed, or moved.
+        return  oldItem.nightId == newItem.nightId
+    }
 
+    override fun areContentsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+        // To change body of created functions use File | Settings | File Templates.
+        // Inside areContentsTheSame(), check whether oldItem and newItem contain the same data;
+        // that is, whether they are equal. This equality check will check all the fields,
+        // because SleepNight is a data class. Data classes automatically define equals and
+        // a few other methods for you. If there are differences between oldItem and newItem,
+        // this code tells DiffUtil that the item has been updated.
+        return oldItem == newItem
+    }
+
+}
 
